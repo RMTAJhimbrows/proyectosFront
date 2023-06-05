aa1e4d3a5a4a8fd3cba95313f8cadb1cc5f6bb5b
RMTAJhimbrows <moitico428@gmail.com>
2023-06-06 00:39:15 +0200

    ajustes de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProductosComponent } from './productos/productos.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './componentes/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NavComponent } from './nav/nav.component';
-import { LoginComponent } from './login/login.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { CardTypeComponent } from './componentes/card-type/card-type.component';
+import { CategoryComponent } from './componentes/category/category.component';
+import { ConfigurationComponent } from './componentes/configuration/configuration.component';
+import { DocumentTypeComponent } from './componentes/document-type/document-type.component';
+import { ExistingProductComponent } from './componentes/existing-product/existing-product.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
+import { RolComponent } from './componentes/rol/rol.component';
+import { SoldProductComponent } from './componentes/sold-product/sold-product.component';
+import { SubCategoryComponent } from './componentes/sub-category/sub-category.component';
+import { UserComponent } from './componentes/user/user.component';
 
 const appRoutes: Routes = [
 
@@ -26,7 +36,17 @@ const appRoutes: Routes = [
     HeaderComponent,
     NavComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    CardTypeComponent,
+    CategoryComponent,
+    ConfigurationComponent,
+    DocumentTypeComponent,
+    ExistingProductComponent,
+    ProfileComponent,
+    RolComponent,
+    SoldProductComponent,
+    SubCategoryComponent,
+    UserComponent
 
   ],
   imports: [
